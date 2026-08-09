1df3dfdc1d0d7cd50f298185dc381c0298499a49
dhruvil012 <111dhruvil111@gmail.com>
2026-08-09 21:37:33 +0530

    Add files via upload

--- CivilCalc-sw.js
+++ CivilCalc-sw.js
@@ -1,5 +1,5 @@
-const CACHE='civilcalc-v71-20260809-r2';
-const SHELL=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-512-maskable.png'];
+const CACHE='civilcalc-v71-20260809-r3-root-icons';
+const SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
